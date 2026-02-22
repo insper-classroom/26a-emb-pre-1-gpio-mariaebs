83fdb08e608fbea3ae6b0ab476c9b4c9b71e05c7
Maria Santos <mariaebs@al.insper.edu.br>
2026-02-22 14:01:20 -0300

    cppcheck4

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -5,18 +5,18 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
-    printf(product);
     return product;
 }
 
 int main(void) {
-    int a=6, b = 5; //uninitvar, legacyUninitvar
+    int a = 6, b = 5;
     int result = multiplyNumbers(a, b);
+    printf("Resultado: %d\n", result); 
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) {
         arr[i] = i;
-        printf(arr[i]);
+        printf("arr[%d] = %d\n", i, arr[i]); 
     }
 
     return 0;

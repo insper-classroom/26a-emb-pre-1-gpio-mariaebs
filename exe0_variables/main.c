8355dd090ee80e14e6da184c476160e11be66187
Maria Santos <mariaebs@al.insper.edu.br>
2026-02-22 15:17:35 -0300

    variaveis4

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -7,11 +7,10 @@ int foo(int a) {
 
 void main(void) {
     int a=0;
-    int b=0;
     while (1) {
         a= foo(a);
         if (a > 5) {
-            b = 1;
+            int b = 1;
             printf("%d/n",b);
         }
     }
